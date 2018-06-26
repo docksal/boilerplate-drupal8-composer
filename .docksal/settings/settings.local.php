89ec554935974f39eca8c72d4596fccb42f2b9c3
Sean Dietrich <sean@kanopistudios.com>
2018-06-26 16:47:03 -0700

    Updated docksal commands

---
+++ .docksal/settings/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = array (
+  'database' => 'default',
+  'username' => 'user',
+  'password' => 'user',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
+$settings['hash_salt'] = 'DDZwah2PG3SuJjqiZfqG6sx_nPVDahRjOJBc_skeyBgU0yOI6hZiCui3DP5hZwP55cWZgSheTg';
